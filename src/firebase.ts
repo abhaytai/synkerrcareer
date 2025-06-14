27b4aa708fb251ff8b8bd8f8b642e7f9e1320fb2
Abhay Tai <122995563+abhaytai@users.noreply.github.com>
2025-06-14 22:01:35 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBF47FELu7Spi9iw6L0RrWTOIggslAiZHg',
-  authDomain: 'synkerr-7011e.firebaseapp.com',
-  projectId: 'synkerr-7011e',
-  storageBucket: 'synkerr-7011e.firebasestorage.app',
-  messagingSenderId: '416750548013',
-  appId: '1:416750548013:web:0fe93a6e0f9a76fe97b4a4',
-  measurementId: 'G-J7XLBT7DDQ',
+  apiKey: 'xyz',
+  authDomain: 'xyz',
+  projectId: 'xyz',
+  storageBucket: 'xyz',
+  messagingSenderId: 'xyz',
+  appId: 'xyz',
+  measurementId: 'xyz',
 };
 
 const app = initializeApp(firebaseConfig);
